86aaa0e5ed0de64a3f06dfc6e1dc397815452036
MadisonReese <madiraylene@outlook.com>
2020-10-02 15:52:15 -0700

    Adding Pods and new features

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Madison R Reese on 9/29/20.
+//  Copyright © 2020 madiraylene. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
